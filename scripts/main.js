e04d79652f9e842a9e0d788b170d8ede0879e852
Mit Sudebov <sudebovds@gmail.com>
2020-05-16 17:12:11 +0300

    Start of day 16.05.2020 - end of main page. Tours in St. Petersburg page

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    const slider = $("[data-slider]");
+    const buttons = $("[data-button]");
+    const popups = $("[data-popup]");
+
+    slider.each(function(){
+        let cur = $(this).data("slider");
+        if(cur == "reviews"){
+            $(this).owlCarousel({
+                items: 2
+            });
+        }
+        else if(cur = "main-slider"){
+            $(this).owlCarousel({
+                items: 1, 
+                autoplay: true,
+                autoplayTimeout: 9000
+            });
+        }
+    });
+
+    $(buttons).on("click", function(){
+        $(popups).addClass("popup-active");
+        $("body").addClass("no-overflow");
+    });
+
+    $(".popup__header_close").on("click", function(){
+        $(popups).removeClass("popup-active");
+        $("body").removeClass("no-overflow");
+    });
+    $(".popup").on("click", function(event){
+        $(popups).removeClass("popup-active");
+        $("body").removeClass("no-overflow");
+    });
+    $(".popup__inner").on("click", function(event){
+        event.stopPropagation();
+    });
+});
+
+  
+
