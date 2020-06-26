9b6f70354c88beabb05ce3ad7c639826a4f0c9bc
sudebovds <sudebovds@gmail.com>
2020-06-26 21:14:05 +0300

    Finish mobile version of main page. New header for over page. Change if to switch/case in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,43 +6,46 @@ $(document).ready(function(){
 
     slider.each(function(){
         let cur = $(this).data("slider");
-        if(cur === "reviews"){
-            $(this).owlCarousel({
-                items: 2, 
-                nav: true,
-                responsive:{
-                    1200: {
-                        items: 2
-                    },
-                    350: {
-                        items: 1,
-                        nav: false
+
+        switch(cur){
+            case "reviews": 
+                $(this).owlCarousel({
+                    items: 2, 
+                    nav: true,
+                    responsive:{
+                        1200: {
+                            items: 2
+                        },
+                        350: {
+                            items: 1,
+                            nav: false
+                        }
                     }
-                }
-            });
-        }
-        if(cur === "main-slider"){
-            $(this).owlCarousel({
-                items: 1, 
-                autoplay: true,
-                autoplayTimeout: 9000,
-                autoWidth: false,
-                center: true,
-                loop: true,
-                nav: true
-            });
-        }
-        if(cur === "detail-sld"){
-            $(this).owlCarousel({
-                items: 1,
-                nav: true
-            });
-        }
-        if(cur === "popular-mobile"){
-            $(this).owlCarousel({
-                items: 1
-            });
-        }      
+                });
+            case "main-slider": 
+                $(this).owlCarousel({
+                    items: 1, 
+                    autoplay: true,
+                    autoplayTimeout: 9000,
+                    autoWidth: false,
+                    center: true,
+                    loop: true,
+                    nav: true
+                });
+
+            case "detail-sld":
+                $(this).owlCarousel({
+                    items: 1,
+                    nav: true
+                });
+
+            case "popular-mobile":
+                $(this).owlCarousel({
+                    items: 1
+                });
+
+            default: return cur = null;
+        };
     });
     
     $(nextSlideArrow).on("click", () => {
